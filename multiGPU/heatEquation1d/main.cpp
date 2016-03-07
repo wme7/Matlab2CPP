a8a05da93b59ed317f9e0077a7d91b5df1c64a32
Manuel Diaz <manuel.ade@gmail.com>
2016-03-07 16:06:52 +0800

    bug in makefile fixed

--- multiGPU/heatEquation1d/main.cpp
+++ multiGPU/heatEquation1d/main.cpp
@@ -5,33 +5,32 @@
 int main() {
   // Initialize varaibles
   float *h_u;
-  float *h_ul;
-  float *t_u;
-  float *t_un;
+  float *d_u;
+  float *d_un;
 
   // Auxiliary variables
   int tid;
   int step;
   time_t t;
 
-  // Initialize memory for h_u 
-  Manage_Memory(0,0,&h_u,$h_ul,&d_u,&d_un);
+  // Initialize memory for h_u and h_ul
+  Manage_Memory(0,0,&h_u,&d_u,&d_un);
 
-  // Set IC in global domain
+  // Set Dirichlet BCs in global domain
   Call_Init(&h_u);
   
   // Set number of threads
   omp_set_num_threads(NO_GPU);
-  #pragma omp parallel shared(h_u) private(tid,t_u,t_un,step) 
+#pragma omp parallel shared(h_u) private(tid,d_u,d_un,step) 
   {
     // Get thread ID
-    tid = omp_get_thread_num();
+    tid = omp_get_thread_num(); printf("tid = %d\n",tid);
 
     // Allocate t_u and t_un for each tid
-    Manage_Memory(1,tid,&h_u,&t_u,&t_un);
+    Manage_Memory(1,tid,&h_u,&d_u,&d_un);
 
     // Set Initial Condition
-    Call_GPU_Init(tid,&t_u);
+    Call_GPU_Init(tid,&d_u);
     #pragma omp barrier
 
     // Request computer current time
@@ -41,20 +40,20 @@ int main() {
     //for (step = 0; step < NO_STEPS; step+=2) {
     //if (step%100==0) printf("Step %d of %d\n",step,(int)NO_STEPS);
       // Communicate Boundaries
-      Manage_Comms(1,tid,&h_u,&t_u);
-      Manage_Comms(2,tid,&h_u,&t_u);
+    Manage_Comms(1,tid,&h_u,&d_u);
+    Manage_Comms(2,tid,&h_u,&d_u);
       
       // Compute stencil
-      Call_Laplace(tid,&t_u,&t_un);
+      //Call_Laplace(tid,&d_u,&d_un);
       #pragma omp barrier
 
       //}
 
     // Copy threads data to global data variable
-      Manage_Comms(3,tid,&h_u,&t_u);
+    Manage_Comms(3,tid,&h_u,&d_u);
 
     // Free memory
-    Manage_Memory(2,tid,&h_u,&t_u,&t_un);
+    Manage_Memory(2,tid,&h_u,&d_u,&d_un);
     #pragma omp barrier
   }
 
@@ -65,7 +64,7 @@ int main() {
   Save_Results(h_u); 
 
   // Free memory on host
-  Manage_Memory(3,0,&h_u,&t_u,&t_un);
+  Manage_Memory(3,0,&h_u,&d_u,&d_un);
 
   return 0;
 }
