9f70f98e08fb3eecd7dad6b1713892d515c97a7c
Manuel Diaz <manuel.ade@gmail.com>
2016-03-19 20:01:52 +0800

    runnig speed tests

--- singleGPU/heatEquation3d/heat3d.h
+++ singleGPU/heatEquation3d/heat3d.h
@@ -4,9 +4,9 @@
 #include <math.h>
 
 #define DEBUG 0 // Display all error messages
-#define NX 256 // number of cells in the x-direction
-#define NY 256 // number of cells in the y-direction
-#define NZ 512 // number of cells in the z-direction
+#define NX 512 // number of cells in the x-direction
+#define NY 512 // number of cells in the y-direction
+#define NZ 1024 // number of cells in the z-direction
 #define L 10.0 // domain length
 #define W 10.0 // domain width
 #define H 20.0 // domain width
@@ -21,11 +21,12 @@
 #define KZ (C*DT/(DZ*DZ)) // numerical conductivity
 #define NO_STEPS (TEND/DT) // No. of time steps
 #define PI 3.1415926535897932f
-#define NI 16 // block size in the i-direction
-#define NJ 16 // block size in the j-direction
-#define NK 4  // block size in the k-direction	
+#define NI 8 // block size in the i-direction
+#define NJ 8 // block size in the j-direction
+#define NK 4 // block size in the k-direction	
 #define I2D(n,i,j) ((i)+(n)*(j)) // transfrom a 2D array index pair into linear index memory
-#define DIVIDE_INTO(x,y) (((x)+(y)-1)/(y)) // define No. of blocks/warps
+//#define DIVIDE_INTO(x,y) (((x)+(y)-1)/(y)) // define No. of blocks/warps
+#define DIVIDE_INTO(x,y) (((x)-1)/(y)) // define No. of blocks/warps
 
 // set USE_CPU to 1 to run only on CPU
 // set USE_GPU to 1 to use GPU kernel - without shared mem
