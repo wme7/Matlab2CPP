a7e4e681d5b6aaf18fa563015d236d1e2d054855
Manuel Diaz <manuel.ade@gmail.com>
2016-04-01 14:28:26 +0800

    delete wrong example

--- MPI/MPI_snipets/other_heat2d/helpers.h
+++
@@ -1,9 +0,0 @@
-typedef struct {
-    float* grid;
-    float diff;
-} result;
-
-void initialize(float** grid, int n, int height, int numProcs, int myID);
-void print(float** grid, int n, int height, int myID);
-void print_buffer(float* buf, int length, int step);
-void handle_mpi_error(int ierr);
