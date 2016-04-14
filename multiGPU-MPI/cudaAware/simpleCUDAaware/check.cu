6f5eb62620bde03d4e8732132eecb945fedcac37
Manuel Diaz <manuel.ade@gmail.com>
2016-04-14 21:35:24 +0800

    minor modifications

--- multiGPU-MPI/cudaAware/simpleCUDAaware/check.cu
+++ multiGPU-MPI/cudaAware/simpleCUDAaware/check.cu
@@ -1,22 +1,32 @@
 #include <mpi.h>
 
-int main( int argc, char *argv[] )
-{
+int main( int argc, char *argv[]){
+  
+  // Initialize variables
   int rank;
   float *ptr = NULL;
   const size_t elements = 32;
   MPI_Status status;
+  int tag = 0;
 
+  // Initialize MPI
   MPI_Init( NULL, NULL );
   MPI_Comm_rank( MPI_COMM_WORLD, &rank );
+
+  // Allocate a single float in device memory
   cudaMalloc( (void**)&ptr, elements * sizeof(float) );
 
-  if( rank == 0 )
-    MPI_Send( ptr, elements, MPI_FLOAT, 1, 0, MPI_COMM_WORLD );
-  if( rank == 1 )
-    MPI_Recv( ptr, elements, MPI_FLOAT, 0, 0, MPI_COMM_WORLD, &status );
+  // Pass element from device 0 to device 1
+  if( rank == 0 ) MPI_Send( ptr, elements, MPI_FLOAT, 1, tag, MPI_COMM_WORLD );
+  if( rank == 1 ) MPI_Recv( ptr, elements, MPI_FLOAT, 0, tag, MPI_COMM_WORLD, &status );
 
+  // clear memory in all devices
   cudaFree( ptr );
+
+  // print output message
+  printf("TEST PASSED\n");
+
+  // Finalize MPI
   MPI_Finalize();
 
   return 0;
