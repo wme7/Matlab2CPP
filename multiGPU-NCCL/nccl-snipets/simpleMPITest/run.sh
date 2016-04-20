7fe2558a8b1c523f65231aa0e1f097dd13c8115c
Manuel Diaz <manuel.ade@gmail.com>
2016-04-20 12:12:52 +0800

    initial commit

--- multiGPU-NCCL/nccl-snipets/simpleMPITest/run.sh
+++ multiGPU-NCCL/nccl-snipets/simpleMPITest/run.sh
@@ -1,3 +1,3 @@
 make clean
 make
-~/openMPI/bin/mpirun -np 2 mpi_test 0 1
+~/openMPI/bin/mpirun -np 2 test.run 0 1
--- multiGPU-NCCL/nccl-snipets/simpleTest/all_gather_test.cu
+++ multiGPU-NCCL/nccl-snipets/simpleTest/all_gather_test.cu
@@ -1,3 +1,30 @@
+/*************************************************************************
+ * Copyright (c) 2015-2016, NVIDIA CORPORATION. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *  * Neither the name of NVIDIA CORPORATION nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ ************************************************************************/
 
 #include <chrono>
 #include <cstdio>
@@ -5,40 +32,37 @@
 #include <string>
 #include <vector>
 
-#include <nccl.h>
+#include "nccl.h"
 #include "test_utilities.h"
 
 
 template<typename T>
-void RunTest(T** buff, const int N, const ncclDataType_t type, const int root,
+void RunTest(T** sendbuff, T** recvbuff, const int N, const ncclDataType_t type,
     ncclComm_t* const comms, const std::vector<int>& dList) {
   // initialize data
   int nDev = 0;
   NCCLCHECK(ncclCommCount(comms[0], &nDev));
   cudaStream_t* s = (cudaStream_t*)malloc(sizeof(cudaStream_t)*nDev);
-  T* buffer = (T*)malloc(N * sizeof(T));
-  T* result = (T*)malloc(N * sizeof(T));
-  memset(result, 0, N * sizeof(T));
+  T* buffer = (T*)malloc(nDev * N * sizeof(T));
+  T* result = (T*)malloc(nDev * N * sizeof(T));
+  memset(buffer, 0, nDev * N * sizeof(T));
+  memset(result, 0, nDev * N * sizeof(T));
 
   for (int i = 0; i < nDev; ++i) {
     CUDACHECK(cudaSetDevice(dList[i]));
     CUDACHECK(cudaStreamCreate(s+i));
+    CUDACHECK(cudaMemset(recvbuff[i], 0, nDev * N * sizeof(T)));
+    Randomize(sendbuff[i], N, i);
 
-    if (i == root) {
-      Randomize(buff[root], N, root);
-      CUDACHECK(cudaMemcpy(result, buff[root], N * sizeof(T),
-          cudaMemcpyDeviceToHost));
-    } else {
-      CUDACHECK(cudaMemset(buff[i], 0, N * sizeof(T)));
-    }
-
-    CUDACHECK(cudaDeviceSynchronize());
+    CUDACHECK(cudaMemcpy(result + i * N, sendbuff[i], N * sizeof(T),
+        cudaMemcpyDeviceToHost));
   }
 
   // warm up GPU
   for (int i = 0; i < nDev; ++i) {
     CUDACHECK(cudaSetDevice(dList[i]));
-    NCCLCHECK(ncclBcast((void*)buff[i], std::min(32 * 1024, N), type, root, comms[i], s[i]));
+    NCCLCHECK(ncclAllGather((const void*)sendbuff[i], std::min(32 * 1024, N), type,
+        (void*)recvbuff[i], comms[i], s[i]));
   }
 
   for (int i = 0; i < nDev; ++i) {
@@ -46,17 +70,17 @@ void RunTest(T** buff, const int N, const ncclDataType_t type, const int root,
     CUDACHECK(cudaStreamSynchronize(s[i]));
   }
 
-//  for (int n = 1; n <= N; n = n << 1)
+  //for (int n = 1; n <= N; n = n << 1)
   {
     int n = N;
-    printf("%12i  %12i  %6s  %4i", (int)(n * sizeof(T)), n,
-        TypeName(type).c_str(), root);
+    printf("%12i  %12i  %6s", (int)(n * sizeof(T)), n, TypeName(type).c_str());
 
     auto start = std::chrono::high_resolution_clock::now();
 
     for (int i = 0; i < nDev; ++i) {
       CUDACHECK(cudaSetDevice(dList[i]));
-      NCCLCHECK(ncclBcast((void*)buff[i], n, type, root, comms[i], s[i]));
+      NCCLCHECK(ncclAllGather((const void*)sendbuff[i], n, type, (void*)recvbuff[i], comms[i],
+          s[i]));
     }
 
     for (int i = 0; i < nDev; ++i) {
@@ -69,21 +93,22 @@ void RunTest(T** buff, const int N, const ncclDataType_t type, const int root,
     double elapsedSec =
         std::chrono::duration_cast<std::chrono::duration<double>>(
             stop - start).count();
-    double algbw = (double)(n * sizeof(T)) / 1.0E9  / elapsedSec;
+    double algbw = (double)(n * sizeof(T)) / 1.0E9 * (double)(nDev - 1)
+        / elapsedSec;
     double busbw = algbw;
 
     double maxDelta = 0.0;
     for (int i = 0; i < nDev; ++i) {
       CUDACHECK(cudaSetDevice(dList[i]));
-      double tmpDelta = CheckDelta<T>(buff[i], result, n);
+      double tmpDelta = CheckDelta<T>(recvbuff[i], result, nDev*N);
       maxDelta = std::max(tmpDelta, maxDelta);
     }
 
     printf("  %7.3f  %5.2f  %5.2f  %7.0le\n", elapsedSec * 1.0E3, algbw, busbw,
-            maxDelta);
+        maxDelta);
   }
 
-  for(int i=0; i < nDev; ++i) {
+  for (int i = 0; i < nDev; ++i) {
     CUDACHECK(cudaSetDevice(dList[i]));
     CUDACHECK(cudaStreamDestroy(s[i]));
   }
@@ -97,29 +122,30 @@ void RunTests(const int N, const ncclDataType_t type, ncclComm_t* const comms,
     const std::vector<int>& dList) {
   int nDev = 0;
   NCCLCHECK(ncclCommCount(comms[0], &nDev));
-  T** buff = (T**)malloc(nDev * sizeof(T*));
+  T** sendbuff = (T**)malloc(nDev * sizeof(T*));
+  T** recvbuff = (T**)malloc(nDev * sizeof(T*));
 
   for (int i = 0; i < nDev; ++i) {
     CUDACHECK(cudaSetDevice(dList[i]));
-    CUDACHECK(cudaMalloc(buff + i, N * sizeof(T)));
+    CUDACHECK(cudaMalloc(sendbuff + i, N * sizeof(T)));
+    CUDACHECK(cudaMalloc(recvbuff + i, nDev * N * sizeof(T)));
   }
 
-  //for (int root = 1; root < 2; ++root) {
-  for (int root = 0; root < nDev; ++root) {
-    RunTest<T>(buff, N, type, root, comms, dList);
-  }
+  RunTest<T>(sendbuff, recvbuff, N, type, comms, dList);
 
   for (int i = 0; i < nDev; ++i) {
     CUDACHECK(cudaSetDevice(dList[i]));
-    CUDACHECK(cudaFree(buff[i]));
+    CUDACHECK(cudaFree(sendbuff[i]));
+    CUDACHECK(cudaFree(recvbuff[i]));
   }
 
-  free(buff);
+  free(sendbuff);
+  free(recvbuff);
 }
 
 void usage() {
-  printf("Tests nccl Broadcast with user supplied arguments.\n"
-      "    Usage: broadcast_test <data size in bytes> [number of GPUs] "
+  printf("Tests nccl AllGather with user supplied arguments.\n"
+      "    Usage: all_reduce_test <data size in bytes> [number of GPUs] "
       "[GPU 0] [GPU 1] ...\n\n");
 }
 
@@ -127,9 +153,9 @@ int main(int argc, char* argv[]) {
   int nVis = 0;
   CUDACHECK(cudaGetDeviceCount(&nVis));
 
-  unsigned long long N = 0;
+  int N = 0;
   if (argc > 1) {
-    int t = sscanf(argv[1], "%llu", &N);
+    int t = sscanf(argv[1], "%d", &N);
     if (t == 0) {
       printf("Error: %s is not an integer!\n\n", argv[1]);
       usage();
@@ -154,6 +180,7 @@ int main(int argc, char* argv[]) {
   for (int i = 0; i < nDev; ++i)
     dList[i] = i % nVis;
 
+
   if (argc > 3) {
     if (argc - 3 != nDev) {
       printf("Error: insufficient number of GPUs in list\n\n");
@@ -171,11 +198,11 @@ int main(int argc, char* argv[]) {
     }
   }
 
-  ncclComm_t* comms = (ncclComm_t*)malloc(sizeof(ncclComm_t)*nDev);;
+  ncclComm_t* comms = (ncclComm_t*)malloc(sizeof(ncclComm_t)*nDev);
   NCCLCHECK(ncclCommInitAll(comms, nDev, dList.data()));
 
   printf("# Using devices\n");
-  for (int g = 0; g < nDev; ++g) {
+  for (int g=0; g<nDev; ++g) {
     int cudaDev;
     int rank;
     cudaDeviceProp prop;
@@ -187,12 +214,12 @@ int main(int argc, char* argv[]) {
   }
   printf("\n");
 
-  printf("# %10s  %12s  %6s  %4s  %7s  %5s  %5s  %7s\n",
-      "bytes", "N", "type", "root", "time", "algbw", "busbw", "delta");
+  printf("# %10s  %12s  %6s  %7s  %5s  %5s  %7s\n",
+      "bytes", "N", "type", "time", "algbw", "busbw", "delta");
 
   RunTests<char>(N / sizeof(char), ncclChar, comms, dList);
   RunTests<int>(N / sizeof(int), ncclInt, comms, dList);
-#ifdef CUDA_HAS_HALF
+#if CUDART_VERSION >= 7050
   RunTests<half>(N / sizeof(half), ncclHalf, comms, dList);
 #endif
   RunTests<float>(N / sizeof(float), ncclFloat, comms, dList);
@@ -202,9 +229,10 @@ int main(int argc, char* argv[]) {
 
   printf("\n");
 
-  for(int i = 0; i < nDev; ++i)
+  for(int i=0; i<nDev; ++i)
     ncclCommDestroy(comms[i]);
   free(comms);
 
   exit(EXIT_SUCCESS);
 }
+
