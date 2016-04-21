bb7662def78ff4c5e78f758ccd5ce6805eabde60
Manuel Diaz <manuel.ade@gmail.com>
2016-04-21 15:38:25 +0800

    exploring UM from CUDA6

---
+++ multiGPU-MPI/cudaAware/UnifiedMemory/example1.cu
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+__global__ void printValue( int *value) {
+printf("value %d\n",value[0]);
+}
+ 
+void hostFunction(){
+int *value;
+cudaMallocManaged(&value, 2 * sizeof(int));
+value[0]=1;
+value[1]=2;
+printValue<<< 1, 1 >>>(value);
+cudaDeviceSynchronize();
+cudaFree(value);
+}
+ 
+int main() {
+hostFunction();
+return 0;
+}
---
+++ multiGPU-MPI/cudaAware/UnifiedMemory/example2.cu
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+__global__ void printValue( int *value) {
+printf("value %d\n",value[0]);
+}
+ 
+void hostFunction(int *value){
+ 
+value[0]=1;
+value[1]=2;
+printValue<<< 1, 1 >>>(value);
+cudaDeviceSynchronize();
+cudaFree(value);
+}
+ 
+int main() {
+int *value;
+cudaMallocManaged(&value, 2 * sizeof(int));
+hostFunction(value);
+return 0;
+}
